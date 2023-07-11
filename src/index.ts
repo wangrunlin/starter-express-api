fea7c792518048cecf67b65f4cad974665ccd502
Leo Wang <leo@wangrunlin.com>
2023-07-11 17:08:26 +0800

    feat: add koa

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
-import express from "express";
+import Koa from "koa";
+import Router from "koa-router";
 
-const app = express();
+const app = new Koa();
+const router = new Router();
 
-app.all("/", (_, res) => {
-  console.log("Just got a request!");
-  res.send("Yo!");
+router.get("/", async (ctx) => {
+  console.log("home log");
+  ctx.body = "Hello World!";
 });
 
-app.get("/test", (_, res) => res.send("This is a test!"));
+router.get("/test", async (ctx) => (ctx.body = "test"));
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+// app.all("/", (_, res) => {
+//   console.log("Just got a request!");
+//   res.send("Yo!");
+// });
+
+// app.get("/test", (_, res) => res.send("This is a test!"));
 
 app.listen(process.env.PORT || 3000);
